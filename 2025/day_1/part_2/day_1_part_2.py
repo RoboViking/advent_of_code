319cd67f47683ae8966aa260be9f4f33b1be6449
Fabian Henrysson <fhenrysson@pop-os.localdomain>
2025-12-01 15:51:37 +0100

    part 2 done

--- 2025/day_1/part_2/day_1_part_2.py
+++ 2025/day_1/part_2/day_1_part_2.py
@@ -1,42 +1,32 @@
 import math
 start = 50
-end = start
-zero_cross_count = 0
-"""
+dialSize = 100
+dialPos = start
+password = 0
+rev = 0
+
 def spin(direction, distance):
-    global start, end
-    
+    global dialPos, password, dialSize
+
     if direction == "R":
-        newPosition = end + distance
+        for _ in range(distance):
+            dialPos += 1
+            if dialPos >= dialSize:
+                dialPos = 0
+                password += 1
+
     elif direction == "L":
-        newPosition = end - distance
-    finalPosition = newPosition % 100
-    end = finalPosition
-
-    print(f"Spun {direction}{distance}: from {start} to {end}")
-    
-"""
-def spin(direction, distance):
-    global end, zero_cross_count
-
-    step = 1 if direction == "R" else -1
-
-    for _ in range(distance):
-        end += step
-        if end >= 100:
-            end = 0
-            zero_cross_count += 1
-        elif end < 0:
-            end = 99
-            zero_cross_count += 1
-
-    print(f"Spun {direction}{distance}: now at {end}, zero crosses so far: {zero_cross_count}")
-
+        for _ in range(distance):
+            dialPos -= 1
+            if dialPos < 0:
+                dialPos = 99
+            
+            if dialPos == 0:
+                password += 1
+            
+    print(f"Spun {direction}{distance}: now at {dialPos}, password: {password}")
 
 def main():
-    startPosition = int(50)
-    currentPosition = startPosition
-    password = int(0)
     with open("day_1/input.txt", "r") as f:
         for line in f:
             line = line.strip()
